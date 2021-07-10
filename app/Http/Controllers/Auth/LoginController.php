ddae15c4181b1c7646fd09d901160f2482dc4406
Marcio Fornari <marcio.fornari@a2mdigital.com.br>
2021-07-10 10:10:28 -0300

    Teste Login dentz

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -112,7 +112,7 @@ class LoginController extends Controller
                 }
            
         }else if (Auth::guard('web')->attempt(['email' => $request->email, 'password' => $request->password])) {
-            dd('attempt');
+         
             //VERIFICAR SE O PARCEIRO ESTÁ INATIVADO
             $tenant_user = Auth::guard('web')->user()->tenant_id;
             $tenant = TenantParceiro::find($tenant_user);
@@ -161,7 +161,7 @@ class LoginController extends Controller
             }
           
         }else{
-            dd('else');
+            
            return redirect()->back()->with('toast_error', trans('auth.failed'));
         }
     }
