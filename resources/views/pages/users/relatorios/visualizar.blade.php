4f4fbd0b906363d6a17e0a3180fdb00c138e5918
Marcio Fornari <marcio.fornari@a2mdigital.com.br>
2022-02-22 17:44:25 -0300

    ajuste objeto filtros e incluir favoritos

--- resources/views/pages/users/relatorios/visualizar.blade.php
+++ resources/views/pages/users/relatorios/visualizar.blade.php
@@ -715,7 +715,7 @@ iframe{
                     navContentPaneEnabled: true
                   }
                 };
-            console.log("Filtros" + config);
+            console.log("Filtros" + JSON.stringify(filtros, null, 4) );
             var embedContainer = $('#powerBI')[0];
             var report = powerbi.embed(embedContainer, config);
         } // FIM FUNCAO ABRIR RELATORIO COM FILTRO
