6b06c9b97fcd976664ebd3bac1491fe09f2c3657
Marcio Fornari <marcio.fornari@a2mdigital.com.br>
2022-09-23 08:06:20 -0300

    debug

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -40,11 +40,9 @@ class AuthController extends Controller
     {
         $credentials = $request->only(['email', 'password']);
        
-        if (!$token = auth('apiParceiro')->setTTL(Carbon::now()->addDays(365)->timestamp)->attempt($credentials)) {
+       // if (!$token = auth('apiParceiro')->setTTL(Carbon::now()->addDays(365)->timestamp)->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
-        }else{
-            return 'tste';
-        }
+        //}
 
         return $this->respondWithTokenParceiro($token);
     }
