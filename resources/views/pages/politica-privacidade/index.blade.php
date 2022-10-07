99db1d63a56ec5a32ca9496826e3f92148e1eb5c
Marcio Fornari <marcio.fornari@a2mdigital.com.br>
2022-10-07 13:40:32 -0300

    Politica de Privacidade

--- resources/views/pages/politica-privacidade/index.blade.php
+++ resources/views/pages/politica-privacidade/index.blade.php
@@ -11,7 +11,7 @@
             A A2m Digital construiu o aplicativo Bi Explorer como
                    um aplicativo gratuito. Este SERVIÇO é prestado por
                    A2m Digital sem custo e destina-se ao uso como
-                   é.
+                   ferramenta de visualização da plataforma BI EXPLORER da A2m Digital.
                 </p> <p>
                   Esta página é usada para informar os visitantes sobre nossos
                    políticas com a coleta, uso e divulgação de
@@ -23,19 +23,16 @@
                    usado para fornecer e melhorar o Serviço. Não usaremos ou compartilharemos suas informações com
                    qualquer pessoa, exceto conforme descrito nesta Política de Privacidade.
                 </p> <p>
-                Os termos usados nesta Política de Privacidade têm os mesmos significados
-                   como em nossos Termos e Condições, que estão acessíveis em
-                   Bi Explorer, a menos que definido de outra forma nesta Política de Privacidade.
                 </p> <p><strong>Coleta e uso de informações</strong></p> <p>
                 Para uma melhor experiência, ao usar nosso Serviço,
-                   pode exigir que você nos forneça certos dados pessoais
+                   pode exigir que você nos forneça certos dados pessoais ou
                    informações identificáveis. A informação que
                    que solicitamos serão retidos por nós e usados conforme descrito nesta política de privacidade.
                 </p> <!----> <p><strong>Dados de Registro</strong></p> <p>
                 Queremos informá-lo que sempre que
                    usar nosso Serviço, em caso de erro no aplicativo
                    coletamos dados e informações (através de terceiros
-                   produtos) em seu telefone chamado Log Data. Esses dados de registro podem
+                   produtos) em seu telefone chamado Log Data. Esses dados de registro podem ser que
                    inclua informações como o protocolo de Internet do seu dispositivo
                    endereço (“IP”), nome do dispositivo, versão do sistema operacional,
                    configuração do aplicativo ao utilizar nosso Serviço,
@@ -47,59 +44,43 @@
                    para o seu navegador a partir dos sites que você visita e são
                    armazenados na memória interna do seu dispositivo.
                 </p> <p>
-                Este Serviço não usa esses “cookies” explicitamente. No entanto,
+                    Este Serviço não usa esses “cookies” explicitamente. No entanto,
                    o aplicativo pode usar código de terceiros e bibliotecas que usam
-                   “cookies” para coletar informações e melhorar seus serviços.
-                   Você tem a opção de aceitar ou recusar esses cookies
-                   e saber quando um cookie está sendo enviado para o seu dispositivo. Se você
-                   optar por recusar nossos cookies, talvez você não consiga usar alguns
-                   partes deste Serviço.
+                   “cookies” que são necessárias para o funcionamento do aplicativo.
                 </p> <p><strong>Provedores de serviço</strong></p> <p>
                 Podemos empregar empresas terceirizadas e
                    indivíduos pelos seguintes motivos:
                 </p> <ul><li>Para facilitar nosso serviço;</li> <li>Para fornecer o Serviço em nosso nome;</li> <li>Para executar serviços relacionados ao Serviço; ou</li> <li>Para nos ajudar a analisar como nosso Serviço é usado.</li></ul> <p>
                 Queremos informar os usuários deste Serviço
-                   que esses terceiros tenham acesso aos seus
-                   Em formação. O motivo é executar as tarefas atribuídas a
-                   eles em nosso nome. No entanto, eles são obrigados a não
-                   divulgar ou usar as informações para qualquer outra finalidade.
-                </p> <p><strong>Security</strong></p> <p>
-                Valorizamos sua confiança em nos fornecer seu
-                   Informações Pessoais, portanto, estamos nos esforçando para usar comercialmente
-                   meios aceitáveis de protegê-la. Mas lembre-se que nenhum método
-                   de transmissão pela internet, ou método de transmissão eletrônica
-                   armazenamento é 100% seguro e confiável, e não podemos
-                   garantir sua segurança absoluta.
+                   que esses terceiros não tem acesso as suas informações.
+                   O motivo é executar as tarefas atribuídas a
+                   eles em nosso nome como correção de bugs ou atualizações.
                 </p> <p><strong>Links para outros sites</strong></p> <p>
-                Este Serviço pode conter links para outros sites. Se você clicar em
+                   Este aplicativo pode conter links para outros sites. Se você clicar em
                    um link de terceiros, você será direcionado para esse site. Observação
                    que esses sites externos não são operados por nós.
                    Portanto, recomendamos fortemente que você revise o
-                   Política de privacidade desses sites. Nós temos
-                   não controla e não assume nenhuma responsabilidade pelo conteúdo,
+                   Política de privacidade desses sites. Nós não temos
+                  nenhum controle e não assumimos nenhuma responsabilidade pelo conteúdo,
                    políticas de privacidade ou práticas de quaisquer sites de terceiros ou
                    Serviços.
                 </p> <p><strong>Privacidade das crianças</strong></p> <!----> <div><p>
-                Nós não coletamos conscientemente pessoalmente
+                    Nós não coletamos conscientemente pessoalmente
                     informações identificáveis ​​de crianças. Nós
                     encorajar todas as crianças a nunca enviarem
                     informações identificáveis ​​através
                     o Aplicativo e/ou Serviços.
                     Incentivamos os pais e responsáveis ​​legais a monitorar
                     uso da Internet de seus filhos e para ajudar a fazer cumprir esta Política, instruindo
-                    seus filhos nunca forneçam informações de identificação pessoal por meio do Aplicativo e/ou Serviços sem a permissão deles. Se você tem motivos para acreditar que uma criança
-                    nos forneceu informações de identificação pessoal por meio do Aplicativo e/ou Serviços,
-                    Por favor, entre em contato conosco. Você também deve ter pelo menos 16 anos de idade para consentir com o processamento
-                    de suas informações de identificação pessoal em seu país (em alguns países, podemos permitir que seus pais
-                    ou guardião para fazê-lo em seu nome).
+                    seus filhos nunca forneçam informações de identificação pessoal por meio do Aplicativo e/ou Serviços sem a permissão deles.
                   </p></div> <p><strong>Alterações nesta Política de Privacidade</strong></p> <p>
                   Podemos atualizar nossa Política de Privacidade de
-                   tempo ao tempo. Assim, você é aconselhado a rever esta página
+                   tempo em tempo. Assim, você é aconselhado a rever esta página
                    periodicamente para quaisquer alterações. Nós vamos
-                   notificá-lo de quaisquer alterações publicando a nova Política de Privacidade em
-                   esta página.
+                   notificá-lo de quaisquer alterações publicando a nova Política de Privacidade nesta
+                    página.
                 </p> <p>Esta política está em vigor a partir de 2022-10-07</p> <p><strong>Contate-nos</strong></p> <p>
-                Se você tiver alguma dúvida ou sugestão sobre nosso
+                Se você tiver alguma dúvida ou sugestão sobre nossa
                    Política de Privacidade, não hesite em nos contatar pelo e-mail a2m@a2mdigital.com.br.
     </body>
     </html>
